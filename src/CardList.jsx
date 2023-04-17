811c7f9a66b99e0b69401413a8c4d530e5f51711
GiorgiGvasalia <ggvasalia4@gmail.com>
2023-04-17 19:59:49 +0400

    initial commit

---
+++ src/CardList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Card from "./Card";
+
+const CardList = ({ robots }) => {
+  const cardComponent = robots.map((robot, i) => {
+    return <Card key={i} id={robot.id} name={robot.name} email={robot.email} />;
+  });
+
+  return <>{cardComponent}</>;
+};
+
+export default CardList;
